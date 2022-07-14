fb90c264ea1a6e6e003185c37bc9c95ffec982d4
Roger Camargo <huogerac@gmail.com>
2022-07-14 04:05:36 -0300

    vamos utilizar Postgres ao invés de SQLite

--- speakerfight/settings.py
+++ speakerfight/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'speakerfight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
     }
 }
 
