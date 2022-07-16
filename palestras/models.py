0428dd1f818234239d71788f1ccd25cefe397b06
Roger Camargo <huogerac@gmail.com>
2022-07-15 22:45:17 -0300

    Cria tabelas iniciais de Evento e Palestra

--- palestras/models.py
+++ palestras/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField
 
-# Create your models here.
+
+class Evento(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=256)
+    slug = AutoSlugField(populate_from='nome', overwrite=True, max_length=200, unique=True, db_index=True)
+    imagem = models.URLField(max_length=1024, null=True, blank=True)
+    site_link = models.URLField(max_length=1024, null=True, blank=True)
+    data_inicio = models.DateTimeField()
+    data_fim = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.user)
+
+
+class Palestra(models.Model):
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='palestras')
+    titulo = models.CharField(max_length=128)
+    descricao = models.TextField()
+    imagem = models.URLField(max_length=1024)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
